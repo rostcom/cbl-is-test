2d894d6f3fa966394cf54f4fb38380bf06c0c3f6
Rostyslav Kobizsky <rostyslav.kobizsky@rozdoum.com>
2014-07-03 14:42:06 +0300

    Init

---
+++ cbl-is-test/CITAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  CITAppDelegate.h
+//  cbl-is-test
+//
+//  Created by Rostyslav on 7/3/14.
+//  Copyright (c) 2014 Rozdoum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CITAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ cbl-is-test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  cbl-is-test
+//
+//  Created by Rostyslav on 7/3/14.
+//  Copyright (c) 2014 Rozdoum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CITAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CITAppDelegate class]));
+    }
+}
